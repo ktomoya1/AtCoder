aa52ef0a61717e414f784ea3a7fc678be0c39a09
tom <134240277+ktomoya1@users.noreply.github.com>
2026-03-28 19:43:31 +0900

    ABC433-C Editorial AC

--- ABC/ABC400-499/ABC433/C/main.cpp
+++ ABC/ABC400-499/ABC433/C/main.cpp
@@ -1,13 +1,38 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <set>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <cmath>
+#include <iomanip>
+#include <limits>
+#include <numeric>
+
 using namespace std;
+using ll = long long;
+
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
 
 int main(void) {
-	string S;
-	cin >> S;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-	int count = 0;
-	for (size_t i = 0; i < S.size(); ++i) {
-		// 
-	}
-	return 0;
+    string s;
+    cin >> s;
+    // 前の文字と現在の文字を比較して違ったら、カウントする
+    int n = s.size();
+    int ans = 0;
+    for (int i = 0; i < n - 1; i++) {
+        if (s[i] + 1 != s[i + 1]) continue;
+        int l = i, r = i + 1;
+        while (l >= 0 && s[l] == s[i]) --l;
+        while (r < n && s[r] == s[i + 1]) ++r;
+        ans += min(i - l, r - (i + 1));
+    }
+    cout << ans << "\n";
+    return 0;
 }
